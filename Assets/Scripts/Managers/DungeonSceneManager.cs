e098ee739ce094f216ece74369c35ff1814052dc
ThunderAwesome <troyjsanders@gmail.com>
2021-04-25 18:05:39 -0500

    Removed duplicate fields (inherited from SceneManager)

--- Assets/Scripts/Managers/DungeonSceneManager.cs
+++ Assets/Scripts/Managers/DungeonSceneManager.cs
@@ -4,33 +4,6 @@ using UnityEngine;
 
 public class DungeonSceneManager : SceneManager
 {
-    #region Properties
-
-    protected int DifficultyLevel { get { return m_currentDifficultyLevel; } }
-
-    public int MaxDifficultyLevel { get { return m_maxDifficultyLevel; } }
-
-    protected float DifficultyFraction => DifficultyLevel / (float)m_maxDifficultyLevel;
-
-    #endregion
-
-    #region Private Fields
-
-    [Header("Components")]
-
-    [SerializeField]
-    private AudioClip m_music = null;
-
-    [SerializeField]
-    private AudioClip m_resultsMusic = null;  
-
-
-    [SerializeField]
-    private int m_maxDifficultyLevel = 5;
-
-    private int m_currentDifficultyLevel = 1;
-       
-    #endregion
 
     #region Protected Methods    
 
